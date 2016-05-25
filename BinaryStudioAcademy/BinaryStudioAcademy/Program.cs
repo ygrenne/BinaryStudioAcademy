92586f141c933ac33ae5ed358729c493b10f188f
ygrenne <ygrenne@gmail.com>
2016-05-25 22:08:31 +0300

    Added people class

--- BinaryStudioAcademy/BinaryStudioAcademy/Program.cs
+++ BinaryStudioAcademy/BinaryStudioAcademy/Program.cs
@@ -10,6 +10,43 @@ namespace BinaryStudioAcademy
     {
         static void Main(string[] args)
         {
+            People peoples = new People();
+
+            peoples.AddPeople(new People("Roma", 26));
+            peoples.AddPeople(new People("Marta", 24));
+
+            Console.WriteLine(string.Join("\n", peoples.GetPeoples()));
+            Console.ReadLine();
+        }
+    }
+
+    class People
+    {
+        private readonly List<People> listOfPeoples = new List<People>();
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public People() { }
+
+        public People(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public List<People> GetPeoples()
+        {
+            return listOfPeoples;
+        }
+
+        public void AddPeople(People people)
+        {
+            listOfPeoples.Add(people);
+        }
+
+        public override string ToString()
+        {
+            return "Name: " + Name + "   Age: " + Age;
         }
     }
 }
